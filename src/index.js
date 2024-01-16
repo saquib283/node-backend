ef15513831fc1ba6688942efe3f970bad7efd5bd
MD REHAN <mdray283@gmail.com>
2024-01-16 17:58:22 +0530

    Stucture for Api request response and middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import app from "./app.js"
 
 dotenv.config({
     path:'./'
 })
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server listening on ${process.env.PORT || 8000}`);
+    })
+})
+.catch()
