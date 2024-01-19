a533735307156d6306c7d29a919b01660addcc96
MD REHAN <mdray283@gmail.com>
2024-01-19 21:14:30 +0530

    integrate user watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -286,7 +286,6 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 });
 
 // Get User Channnel Profile
-
 const getUserChannelProfile = asyncHandler(async(req, res) => {
   const {username} = req.params
 
@@ -359,6 +358,63 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
   )
 })
 
+// Get User watchHistory 
+const getUserWatchHistory = asyncHandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+        $match: {
+            _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+    },
+    {
+        $lookup: {
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner",
+                        pipeline: [
+                            {
+                                $project: {
+                                    fullName: 1,
+                                    username: 1,
+                                    avatar: 1
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    $addFields:{
+                        owner:{
+                            $first: "$owner"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+])
+
+return res
+.status(200)
+.json(
+    new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+    )
+)
+})
+
+
+
 
 
 
@@ -373,5 +429,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getUserWatchHistory
 };
